0627b716fa259420b2249c3122d81cb8171b8aa1
bkharade <46837210+bkharade@users.noreply.github.com>
2024-06-02 19:34:33 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 rm -r demo-r-ecs.tar.gz
 docker build --no-cache -t demo-r-ecs:latest -f Dockerfile .
 docker save demo-r-ecs:latest | gzip > demo-r-ecs.tar.gz
-docker rmi $(docker images -f "dangling=true" -q)
+#docker rmi $(docker images -f "dangling=true" -q)
