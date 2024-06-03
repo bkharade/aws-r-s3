5c3ea3fef0e14e57ef6788fd036f0aed896efbaf
kharade <meetbbk@gmail.com>
2024-06-03 11:12:00 -0400

    updated code

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-rm -r demo-r-ecs.tar.gz
-docker build --no-cache -t demo-r-ecs:latest -f Dockerfile .
-docker save demo-r-ecs:latest | gzip > demo-r-ecs.tar.gz
+rm -r demo-r-s3.tar.gz
+docker build --no-cache -t demo-r-s3 -f Dockerfile .
+docker save demo-r-s3 | gzip > demo-r-s3.tar.gz
 #docker rmi $(docker images -f "dangling=true" -q)
